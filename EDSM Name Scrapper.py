5d136242756688f4ff7caffb27e2cd293a0b06e8
Scaleios <47495861+Scaleios@users.noreply.github.com>
2019-08-25 07:24:49 +0300

    Update EDSM Name Scrapper.py
    Added which expedition each person is on.

--- EDSM Name Scrapper.py
+++ EDSM Name Scrapper.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs4
+from urllib.parse import unquote
 
-raw_html = requests.get("https://www.edsm.net/en/expeditions").content
-html = bs4(raw_html, "html.parser")
 
+a = True
+i = 1
 links = []
-for link in html.find_all("a"):
-    formated = link.get("href")
-    if formated is not None:
-        if "/en/expeditions/summary/" in formated:
-            if formated not in links:
-                links.append(formated)
+while a:
+    raw_html = requests.get(f"https://www.edsm.net/en/expeditions/p/{i}").content
+    if raw_html == requests.get(f"https://www.edsm.net/en/expeditions/p/{i-1}").content or i == 1:
+        html = bs4(raw_html, "html.parser")
+        for link in html.find_all("a"):
+            formated = link.get("href")
+            if formated is not None:
+                if "/en/expeditions/summary/" in formated:
+                    if formated not in links:
+                        links.append(formated)
+        i += 1
+    else:
+        a = False
 
 ongoing = []
 for link in links:
@@ -19,8 +27,10 @@ for link in links:
     if "This expedition is finished." not in html.get_text():
         ongoing.append(link)
 
-names = []
+names = {}
 for link in ongoing:
+    expedition = link[link.find("name") + 5:]
+    expedition_clean = unquote(expedition).replace("+", " ")
     link = link.replace("summary", "participants")
     raw_html = requests.get(f"https://www.edsm.net/{link}").content
     html = bs4(raw_html, "html.parser")
@@ -30,8 +40,10 @@ for link in ongoing:
             if "/en/user/profile/" in formated:
                 spot = formated.find("cmdr")
                 name = formated[spot + 5:]
-                name_clean = name.replace("+", " ")
+                name_clean = unquote(name).replace("+", " ")
                 if name_clean not in names:
-                    names.append(name_clean)
+                    names[name_clean] = []
+                names[name_clean].append(expedition_clean)
+
 
 print(names)
